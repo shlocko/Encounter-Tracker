e5d3a6720fdab79b0eac38a47860711e4856580e
Andrew <mcdude22@gmail.com>
2024-08-07 10:23:49 -0700

    Configured express to server /dist/ as static website

--- server/app.ts
+++ server/app.ts
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = 8080;
@@ -18,10 +19,22 @@ app.listen(PORT, () => {
     console.log("Server started!");
 })
 
+
+app.use((req, res, next) => {
+    if (/(.ico|.js|.css|.jpg|.png|.map)$/i.test(req.path)) {
+        next();
+    } else {
+        res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+        res.header('Expires', '-1');
+        res.header('Pragma', 'no-cache');
+        res.sendFile(path.join(__dirname, "..", "dist", 'index.html'));
+    }
+});
+
+app.use(express.static(path.join(__dirname, "..", "dist")));
+
+
+
 app.get("/api/items", (req, res) => {
     res.send(items);
-})
-
-app.get("/", (req, res) => {
-    res.sendFile(__dirname, "../dist/index.html");
 })
